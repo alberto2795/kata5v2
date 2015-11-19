2f38e7137cd1b37ecb25730b70a86716be496bdb
usuario <usuario@10.22.143.7>
2015-11-19 10:24:27 +0000

    version 2 kata 5v2 lee los emails de la base de datos pasada por parámetro

--- src/KATA5V2.java
+++ src/KATA5V2.java
@@ -7,7 +7,7 @@ public class KATA5V2 {
     public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         //String nameFile = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\KATA5V2\\emailsfilev1.txt";    
         //ArrayList<String> mailArray = MailListReader.read(nameFile);
-        ArrayList<String> mails = new MailReader("KATA:DB").execute();
+        ArrayList<String> mails = new MailReader("KATA.DB").execute();
         Histogram<String> histogram = MailHistogramBuilder.build(mails);
         new HistogramDisplay(histogram).exectute();
     }
